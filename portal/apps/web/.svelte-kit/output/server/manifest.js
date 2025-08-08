ec75bd2385e58b365eed4e2a8464af47ff4992ca
verlyn13 <jeffreyverlynjohnson@gmail.com>
2025-08-07 22:06:48 -0800

    Fix workflow and add correct build output

--- portal/apps/web/.svelte-kit/output/server/manifest.js
+++ portal/apps/web/.svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["CNAME","favicon.svg"]),
 	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.BEf_Lvr5.js",app:"_app/immutable/entry/app.C0cVGRTA.js",imports:["_app/immutable/entry/start.BEf_Lvr5.js","_app/immutable/chunks/Xa-QecMn.js","_app/immutable/chunks/BHMwHsqE.js","_app/immutable/chunks/CDWPIt_c.js","_app/immutable/entry/app.C0cVGRTA.js","_app/immutable/chunks/BjAhemba.js","_app/immutable/chunks/CDWPIt_c.js","_app/immutable/chunks/BHMwHsqE.js","_app/immutable/chunks/Bzak7iHL.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.a6YKIiM3.js",app:"_app/immutable/entry/app.JayedpDV.js",imports:["_app/immutable/entry/start.a6YKIiM3.js","_app/immutable/chunks/m0gLTz_v.js","_app/immutable/chunks/BHMwHsqE.js","_app/immutable/chunks/CDWPIt_c.js","_app/immutable/entry/app.JayedpDV.js","_app/immutable/chunks/BjAhemba.js","_app/immutable/chunks/CDWPIt_c.js","_app/immutable/chunks/BHMwHsqE.js","_app/immutable/chunks/Bzak7iHL.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- portal/apps/web/.svelte-kit/output/server/nodes/1.js
+++ portal/apps/web/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.C3u7JWlS.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/CyKQDHEQ.js","_app/immutable/chunks/CDWPIt_c.js","_app/immutable/chunks/BHMwHsqE.js","_app/immutable/chunks/Bt7a5Wb6.js","_app/immutable/chunks/Xa-QecMn.js"];
+export const imports = ["_app/immutable/nodes/1.e-sbmpqM.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/CyKQDHEQ.js","_app/immutable/chunks/CDWPIt_c.js","_app/immutable/chunks/BHMwHsqE.js","_app/immutable/chunks/Bt7a5Wb6.js","_app/immutable/chunks/m0gLTz_v.js"];
 export const stylesheets = [];
 export const fonts = [];
